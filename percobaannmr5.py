db8e5a7e10a43b08bdfc86d6f06b692d95d6d379
Rianda Rahma Dewanty <120345499+rianddew@users.noreply.github.com>
2026-05-31 00:05:54 +0700

    Add files via upload

---
+++ percobaannmr5.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, timedelta
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root',database='perbankan')
+cursor = cnx.cursor()
+query = ("SELECT id_nasabahFK,jenis_transaksi, tanggal, jumlah FROM transaksi WHERE YEAR(tanggal) = '2022'")
+cursor.execute(query)
+for (id_nasabahFK, jenis_transaksi, tanggal,jumlah) in cursor:
+    print("nasabah dengan ID {} melakukan transaksi {} pada {:%d %b %Y} sejumlah {}"
+          .format(id_nasabahFK, jenis_transaksi, tanggal, jumlah))
+
+cursor.close()
+cnx.close()
